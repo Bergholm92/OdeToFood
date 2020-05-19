7311746508df7d77cecd3e6b32022ebe544445bb
Gustav Bergholm Hansson <gustav.bergholm@live.se>
2020-05-19 14:56:57 +0200

    Binding to a Query String

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -28,10 +28,12 @@ namespace OdeToFood.Pages.Restaurants
 
 
 
-        public void OnGet()
+        public void OnGet(string searchTerm)
         {
+            
+
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
         }
     }
 }
