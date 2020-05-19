35c8958796a43923de95ff6555b6f889e57c1046
Gustav Bergholm Hansson <gustav.bergholm@live.se>
2020-05-19 12:07:32 +0200

    Before Registering a Data Service Lesson

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,43 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using static OdeToFood.Core.Restaurant;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+
+    }
+
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant{ Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant{Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian},
+                new Restaurant{Id = 3, Name = "La Costa", Location = "La Costa", Cuisine=CuisineType.Mexican}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+
+
+
+
+}
