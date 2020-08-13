08b23a5a6df2baa93997cd3b9642bfb643f904d5
Gustav Bergholm Hansson <gustav.bergholm@live.se>
2020-08-13 12:51:40 +0200

    Klar med uppbyggnanden av Edit-sidan

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using static OdeToFood.Core.Restaurant;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+
+        public IEnumerable<SelectListItem> Cuisines{ get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+
+        /// <summary>
+        /// Den här funktionen kontrollerar att det finns en resturang som det resturangId som behandlas,
+        /// Om den restaurangen inte finns, så skickas användaren istället till sidan NotFound. 
+        /// </summary>
+        /// <param name="restaurantId"></param>
+        /// <returns></returns>
+        public IActionResult OnGet(int restaurantId)
+        {
+
+
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
